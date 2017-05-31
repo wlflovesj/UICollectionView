8842f0a7ffc21572cf9695e54a3fa665691f4526
wanglongfei <wanglongfei@wanglongfeidemacbook-pro.local>
2017-05-31 10:15:46 +0800

    Initial Commit

---
+++ UICollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICollectionView
+//
+//  Created by wanglongfei on 2017/5/31.
+//  Copyright © 2017年 wanglongfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UICollectionView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UICollectionView
+//
+//  Created by wanglongfei on 2017/5/31.
+//  Copyright © 2017年 wanglongfei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
